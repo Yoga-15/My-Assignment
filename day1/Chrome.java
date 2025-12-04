1f76197a48f6ccc6944a562884ea161581fa788a
Yoga-15 <yogalakshmi.0515@gmail.com>
2025-12-04 09:22:16 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float versionoftype=91.0f;
+		String developeroftype="Google";
+		boolean betaoftype = false;
+		int releaseyearoftype = 2008;
+		char shortcutKeyoftype = 'C';
+		
+		System.out.println(versionoftype);
+		System.out.println(developeroftype);
+		System.out.println(betaoftype);
+		System.out.println(releaseyearoftype);
+		System.out.println(shortcutKeyoftype);
+	}
+
+}
